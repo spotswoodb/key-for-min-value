4395c25e839be06d5787ead419c09fbd4894ad21
Ben Spotswood <spotswoodb@gmail.com>
2020-11-19 22:07:42 -0600

    commit tree 0bdb30a0a01b76841035dbca85c5b53ae2f09e54

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    def key_for_min_value(name_hash)
+        if name_hash.empty?
+            return nil
+        end
+        min = 0
+        item = ""
+        name_hash.each do |key, value|
+            if min == 0 
+                min = value
+                item = key
+            elsif value < min
+                min = value
+                item = key
+            end
+        end
+        return item
+    end
 end
